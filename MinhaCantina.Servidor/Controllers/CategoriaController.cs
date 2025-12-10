b43f4578fe77adde348a378e6543e9c272a17735
Gaaldra <55445484+Gaaldra@users.noreply.github.com>
2025-12-10 13:38:11 -0300

    Cria o controlador da Categoria

--- MinhaCantina.Servidor/Controllers/CategoriaController.cs
+++ MinhaCantina.Servidor/Controllers/CategoriaController.cs
@@ -1,16 +1,79 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MinhaCantina.Biblioteca.Modelos;
+using MinhaCantina.Servidor.Dados;
+using MySqlConnector;
 
 namespace MinhaCantina.Servidor.Controllers;
 
 [Route("[controller]")]
 [ApiController]
-public class CategoriaController : ControllerBase
+public class CategoriaController(MinhaCantinaContexto contextoCantina) : ControllerBase
 {
-	[HttpGet]
-	public IActionResult PegarCategoria()
+	private MinhaCantinaContexto _contexto = contextoCantina;
+	// Criar uma propriedade privada do tipo MinhaCantinaContexto
+	// O nome da propriedade será _contexto;
+
+	// [Atributo]
+	[HttpPost("criar")]
+	public IActionResult CriarCategoria([FromBody] CategoriaRegistroDto requisicao)
+	{		
+		var categoria = Categoria.Criar(requisicao.Nome.ToUpper());
+
+		try
+		{
+			_contexto.Categorias.Add(categoria);
+			_contexto.SaveChanges();
+		}
+		catch (DbUpdateException excecao)
+		{
+			var excecaoInterna = excecao.InnerException;
+
+			if(excecaoInterna is MySqlException excecaoMySql)
+			{
+				if (excecaoMySql.Number == 1062)
+				{
+					return StatusCode(400, "Essa categoria já existe");
+				}
+			}
+		}
+		catch (Exception excecao)
+		{
+			return StatusCode(500, $"Ocorreu um erro inesperado: {excecao.Message}");
+		}
+
+		return StatusCode(201, categoria);
+	}
+
+	// [Atributo]
+	[HttpGet("pegar/{id}")] // -> Verbo HTTP
+	public IActionResult PegarCategoria(int id)
 	{
-		var categoria = Categoria.Criar("Salgados");
+		var categoria = _contexto.Categorias.Find(id);
+
+		if (categoria is null)
+		{
+			return StatusCode(404, "Categoria não encontrada");
+		}
+
 		return StatusCode(200, categoria);
 	}
+
+	[HttpGet("pegar_todos")]
+	public IActionResult PegarTodasCategorias()
+	{
+		var categorias = _contexto.Categorias.ToList();
+		return StatusCode(200, categorias);
+	}
+}
+
+public class CategoriaRegistroDto
+{
+	public string Nome { get; set; } // Propriedade
+}
+
+public class CategoriaRespostaDto
+{
+	public int Id { get; set; }
+	public string Nome { get; set; }
 }
